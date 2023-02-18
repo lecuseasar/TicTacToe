9f34b983b295c098d9e1f858f1a97e11695136d8
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2023-02-18 19:08:47 +0300

    Initial Commit

---
+++ TicTacToe/ContentView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  TicTacToe
+//
+//  Created by emre argana on 16.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumansTurn = true
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                LazyVGrid(columns: columns, spacing: 5) {
+                    ForEach(0..<9, id: \.self){ index in
+                        ZStack {
+                            Circle()
+                                .foregroundColor(.red)
+                                .opacity(0.5)
+                                .frame(width: geometry.size.width / 3 - 15 ,
+                                       height: geometry.size.width / 3 - 15)
+                            
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: geometry.size.width/10,
+                                       height: geometry.size.width/10)
+                                .foregroundColor(.white)
+                        }
+                        .onTapGesture {
+                            moves[index] = Move(player: isHumansTurn ? .human: .computer, boardIndex: index)
+                            isHumansTurn.toggle() //isHumansTurn = !isHumansTurn
+                        }
+                    }
+                }
+                Spacer()
+            }
+            .padding()
+        }
+    }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex : Int
+    
+    var indicator: String {
+        player == .human ? "xmark" : "circle"
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by emre argana on 16.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
